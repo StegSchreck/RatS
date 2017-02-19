089b83be1e248081465652c92fe7c369d3e19a2c
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2017-02-19 17:21:07 +0100

    fixed test

--- tests/unit/data/test_file_handler.py
+++ tests/unit/data/test_file_handler.py
@@ -8,7 +8,7 @@ from RatS.data.movie import Movie
 TESTDATA_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'assets', 'exports'))
 
 
-class MovieTest(TestCase):
+class FileHandlerTest(TestCase):
     def setUp(self):
         self.movie = Movie()
         self.movie.title = 'Fight Club'
@@ -38,10 +38,11 @@ class MovieTest(TestCase):
 
     def test_save_single_movie_to_file(self):
         movies = [self.movie]
+        movies_json = [m.to_json() for m in movies]
         filename = os.path.join(TESTDATA_PATH, 'TEST_single_movie.json')
         file_handler.save_movies_json(movies, filename)
         with open(filename) as file:
-            self.assertEqual(movies, json.load(file))
+            self.assertEqual(movies_json, json.load(file))
         os.remove(filename)
 
     def test_save_multiple_movies_to_file(self):
@@ -55,8 +56,9 @@ class MovieTest(TestCase):
         movie2.trakt.overall_rating = '89%'
 
         movies = [self.movie, movie2]
+        movies_json = [m.to_json() for m in movies]
         filename = os.path.join(TESTDATA_PATH, 'TEST_multiple_movies.json')
         file_handler.save_movies_json(movies, filename)
         with open(filename) as file:
-            self.assertEqual(movies, json.load(file))
+            self.assertEqual(movies_json, json.load(file))
         os.remove(filename)
