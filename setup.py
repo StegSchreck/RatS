b0be18aa046c77a2fa504a79eff2dc9cead5bb0c
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2017-02-24 18:02:21 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    project_license = f.read()
+
+setup(
+    name='RatS',
+    version='0.1.0',
+    description='Movie ratings synchronisation',
+    long_description=readme,
+    author='Sebastian Schreck',
+    author_email='sebastian@schreck.berlin',
+    url='https://github.com/StegSchreck/RatS',
+    license=project_license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    py_modules=['RatS']
+)
