9f0c4aa24a8dec153a444f363cb24f77ff7ccf28
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2017-02-25 13:50:22 +0100

    fix movielense_inserter test

--- tests/unit/inserters/test_movielense_inserter.py
+++ tests/unit/inserters/test_movielense_inserter.py
@@ -1,10 +1,15 @@
 from unittest import TestCase
+from unittest.mock import patch
 
 from RatS.inserters.movielense_inserter import MovielenseInserter
 from RatS.sites.movielense_site import Movielense
 
 
 class MovielenseInserterTest(TestCase):
-    def test_init(self):
-        inserter = MovielenseInserter()
-        self.assertTrue(Movielense, type(inserter.site))
+
+    @patch('RatS.inserters.base_inserter.Inserter.__init__')
+    @patch('RatS.sites.base_site.PhantomJS')
+    def test_init(self, browser_mock, base_init_mock):
+        MovielenseInserter()
+
+        self.assertTrue(base_init_mock.called)
