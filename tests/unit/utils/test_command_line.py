aa9df8b874569df44c8e84bf31cd1d2eecc3ba8e
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2017-02-25 14:58:27 +0100

    made test method static

--- tests/unit/utils/test_command_line.py
+++ tests/unit/utils/test_command_line.py
@@ -5,7 +5,8 @@ from RatS.utils.command_line import print_progress
 
 
 class CommandLineUtilTest(TestCase):
+    @staticmethod
     @patch('RatS.utils.command_line.get_command_line_dimensions')
-    def test_progress_output(self, os_mock):
+    def test_progress_output(os_mock):
         os_mock.return_value = (50, 50)
         print_progress(1, 100)
