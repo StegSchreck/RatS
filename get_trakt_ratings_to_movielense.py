183885a0901a35508f3b2fb12ca2ce0c0b63020a
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2017-02-26 14:06:49 +0100

    refactoring

--- get_trakt_ratings_to_movielense.py
+++ get_trakt_ratings_to_movielense.py
@@ -13,15 +13,35 @@ EXPORTS_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__), 'RatS',
 JSON_FILE = TIMESTAMP + '_trakt.json'
 CSV_FILE = TIMESTAMP + '_trakt.csv'
 
-if __name__ == "__main__":
-    trakt_parser = TraktRatingsParser()
-    movies = trakt_parser.parse()
+
+def main():
+    # PARSING DATA
+    movies = parse_data_from_source(TraktRatingsParser())
+    # FILE LOADING
+    # movies = load_data_from_file('20170224211816_trakt.json')
+    # POSTING THE DATA
+    insert_movie_ratings(MovielenseInserter(), movies)
+
+
+def parse_data_from_source(parser):
+    movies = parser.parse()
     file_handler.save_movies_json(movies, EXPORTS_FOLDER, JSON_FILE)
-    sys.stdout.write('\r\n===== saved %i parsed movies to %s/%s =====\r\n' % (len(movies), EXPORTS_FOLDER, JSON_FILE))
+    sys.stdout.write('\r\n===== saved %i parsed movies from %s to %s/%s =====\r\n' %
+                     (len(movies), type(parser.site).__name__, EXPORTS_FOLDER, JSON_FILE))
     sys.stdout.flush()
-    # movies = file_handler.load_movies_json(os.path.join(EXPORTS_FOLDER, '20170224211816_trakt.json'))
-    # sys.stdout.write('\r\n===== loaded %i movies from %s/%s =====\r\n' %
-    #                  (len(movies), EXPORTS_FOLDER, '20170224211816_trakt.json'))
-    # sys.stdout.flush()
-    movielense_inserter = MovielenseInserter()
-    movielense_inserter.insert(movies)
+    return movies
+
+
+def load_data_from_file(filename):
+    movies = file_handler.load_movies_json(os.path.join(EXPORTS_FOLDER, filename))
+    sys.stdout.write('\r\n===== loaded %i movies from %s/%s =====\r\n' % (len(movies), EXPORTS_FOLDER, filename))
+    sys.stdout.flush()
+    return movies
+
+
+def insert_movie_ratings(inserter, movies):
+    inserter.insert(movies)
+
+
+if __name__ == "__main__":
+    main()
