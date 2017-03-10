953ceeb7b0a28d31612127c334af9e30dfff632f
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2017-03-10 22:15:52 +0100

    that one is new

--- RatS/utils/file_impex.py
+++ RatS/utils/file_impex.py
@@ -30,5 +30,4 @@ def wait_for_file_to_exist(filepath, seconds=30):
                 return file
         except IOError:
             time.sleep(1)
-    else:
-        raise IOError('Could not access {} after {} seconds'.format(filepath, str(seconds)))
+    raise IOError('Could not access {} after {} seconds'.format(filepath, str(seconds)))
