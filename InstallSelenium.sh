be0025cc3f56270b984ad4f8b4ab480867da99a4
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2017-03-26 15:43:46 +0200

    upgrade to Gockdriver v0.15.0

--- InstallSelenium.sh
+++ InstallSelenium.sh
@@ -1,11 +1,12 @@
+#!/bin/bash
 ## adapted from https://gist.github.com/tristandostaler/2f8b28f2bf503db4422a5549e8fed538
 
 mkdir /usr/bin
 cd /usr/bin
 
-wget https://github.com/mozilla/geckodriver/releases/download/v0.14.0/geckodriver-v0.14.0-linux64.tar.gz
-tar -xvzf geckodriver-v0.14.0-linux64.tar.gz
-rm geckodriver-v0.14.0-linux64.tar.gz
+wget https://github.com/mozilla/geckodriver/releases/download/v0.15.0/geckodriver-v0.15.0-linux64.tar.gz
+tar -xvzf geckodriver-v0.15.0-linux64.tar.gz
+rm geckodriver-v0.15.0-linux64.tar.gz
 chmod +x geckodriver
 cp geckodriver wires
 
