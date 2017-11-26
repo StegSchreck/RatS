8d72c345f7afe1d8d04e63836613b35b3fb14c4e
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2017-11-26 16:42:42 +0100

    fix linting by extracting parts of progress bar creation - #16

--- RatS/utils/command_line.py
+++ RatS/utils/command_line.py
@@ -15,7 +15,7 @@ def print_progress_bar(iteration, total, start_timestamp, prefix='', suffix=''):
         prefix              - Optional  : prefix string (Str)
         suffix              - Optional  : suffix string (Str)
     """
-    _, tty_columns = get_command_line_dimensions()
+    _, tty_columns = _get_command_line_dimensions()
     length_of_percentage_output = 6
     length_of_spaces = 6
 
@@ -25,24 +25,17 @@ def print_progress_bar(iteration, total, start_timestamp, prefix='', suffix=''):
         progress_factor = iteration / float(total)
 
     absolute_progress = str(iteration) + "/" + str(total)
-    time_so_far = time.time() - start_timestamp
-    remaining_time_estimation = (time_so_far / iteration) * total
-    remaining_hours = int(remaining_time_estimation // (60 * 60))
-    remaining_minutes = int((remaining_time_estimation % (60 * 60)) // 60)
-    remaining_seconds = int(remaining_time_estimation % 60)
-    remaining_time = "{}:{:02d}:{:02d}".format(remaining_hours, remaining_minutes, remaining_seconds)
-    percents = "{0:.1f}".format(100 * progress_factor)
+    remaining_time = _estimate_remaining_time(start_timestamp, iteration, total)
+    percents = '{0:.1f}%'.format(100 * progress_factor)
 
     bar_length = int(tty_columns) - len(prefix) - len(absolute_progress) - len(remaining_time)\
         - len(suffix) - length_of_percentage_output - length_of_spaces
-    filled_length = int(round(bar_length * progress_factor))
-    filled_bar = '#' * filled_length + '-' * (bar_length - filled_length)
 
-    sys.stdout.write('\r{prefix} {absolute_progress} {remaining_time} [{filled_bar}] {percents}% {suffix}'.format(
+    sys.stdout.write('\r{prefix} {absolute_progress} {remaining_time} {progress_bar} {percents} {suffix}'.format(
         prefix=prefix,
         absolute_progress=absolute_progress,
         remaining_time=remaining_time,
-        filled_bar=filled_bar,
+        progress_bar=_generate_progress_bar(bar_length, progress_factor),
         percents=percents,
         suffix=suffix
     ))
@@ -52,7 +45,23 @@ def print_progress_bar(iteration, total, start_timestamp, prefix='', suffix=''):
     sys.stdout.flush()
 
 
-def get_command_line_dimensions():
+def _generate_progress_bar(bar_length, progress_factor):
+    filled_length = int(round(bar_length * progress_factor))
+    filled_bar = '#' * filled_length
+    unfilled_bar = '-' * (bar_length - filled_length)
+    return '[{}{}]'.format(filled_bar, unfilled_bar)
+
+
+def _estimate_remaining_time(start_timestamp, iteration, total):
+    time_so_far = time.time() - start_timestamp
+    remaining_time_estimation = (time_so_far / iteration) * total
+    remaining_hours = int(remaining_time_estimation // (60 * 60))
+    remaining_minutes = int((remaining_time_estimation % (60 * 60)) // 60)
+    remaining_seconds = int(remaining_time_estimation % 60)
+    return '{}:{:02d}:{:02d}'.format(remaining_hours, remaining_minutes, remaining_seconds)
+
+
+def _get_command_line_dimensions():
     return os.popen('stty size', 'r').read().split()
 
 
--- tests/unit/utils/test_command_line.py
+++ tests/unit/utils/test_command_line.py
@@ -7,7 +7,7 @@ from RatS.utils.command_line import print_progress_bar
 
 class CommandLineUtilTest(TestCase):
     @staticmethod
-    @patch('RatS.utils.command_line.get_command_line_dimensions')
+    @patch('RatS.utils.command_line._get_command_line_dimensions')
     def test_progress_output(os_mock):
         os_mock.return_value = (50, 50)
         print_progress_bar(
