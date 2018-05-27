f633119083f45d7533a2fc9cfc321cdb3dfcf8cc
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2018-05-27 11:34:07 +0200

    remove hardcoded shit

--- RatS/tmdb/tmdb_ratings_inserter.py
+++ RatS/tmdb/tmdb_ratings_inserter.py
@@ -7,7 +7,9 @@ from RatS.tmdb.tmdb_site import TMDB
 class TMDBRatingsInserter(RatingsUploader):
     def __init__(self, args):
         super(TMDBRatingsInserter, self).__init__(TMDB(args), args)
-        self.url_for_csv_file_upload = 'https://www.themoviedb.org/account/StegSchreck/import'
+        self.url_for_csv_file_upload = 'https://www.themoviedb.org/account/{username}/import'.format(
+            username=self.site.USERNAME
+        )
         self.css_id_of_file_input_element = 'csv_file'
         self.xpath_selector_for_submit_button = "//form[@name='import_csv']//input[@type='submit']"
 
