841642765513fddafd50952b8615cddb7a0b2fe6
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2019-07-29 21:14:40 +0200

    RottenTomates setup - refs #73

---
+++ tests/functional/sites/test_rottentomatoes_site.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+from unittest import skip
+
+from RatS.rottentomatoes.rottentomatoes_site import RottenTomatoes
+
+
+@skip('this test is unstable on travis')
+class RottenTomatoesSiteTest(TestCase):
+    def setUp(self):
+        self.site = RottenTomatoes(None)
+
+    def test_login(self):
+        self.assertIn(self.site.USERNAME, self.site.browser.page_source)
+
+    def tearDown(self):
+        self.site.browser_handler.kill()
