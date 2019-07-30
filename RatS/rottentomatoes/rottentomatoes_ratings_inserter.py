1f24b15f0d4b4f7da09842bdb63a67536e967b21
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2019-07-30 11:42:44 +0200

    RottenTomates Inserter - refs #73

---
+++ RatS/rottentomatoes/rottentomatoes_ratings_inserter.py
@@ -0,0 +1,50 @@
+import time
+import urllib.parse
+
+from RatS.base.base_ratings_inserter import RatingsInserter
+from RatS.rottentomatoes.rottentomatoes_site import RottenTomatoes
+
+
+class RottenTomatoesRatingsInserter(RatingsInserter):
+    def __init__(self, args):
+        super(RottenTomatoesRatingsInserter, self).__init__(RottenTomatoes(args), args)
+
+    def _search_for_movie(self, movie):
+        search_url = 'https://www.rottentomatoes.com/api/private/v2.0/search?{search_params}'.format(
+            search_params=urllib.parse.urlencode({'q': movie['title'], 't': 'movie'})
+        )
+        self.site.browser.get(search_url)
+
+    def _get_search_results(self, search_result_page):
+        json_data = self.site.get_json_from_html()
+        return json_data['movies']
+
+    def _is_requested_movie(self, movie, search_result):
+        if self._is_field_in_parsed_data_for_this_site(movie, 'url'):
+            return movie[self.site.site_name.lower()]['url'] == \
+                   'https://www.rottentomatoes.com' + search_result['url']
+        elif search_result['year'] and movie['year'] == int(search_result['year']):
+            self.site.browser.get('https://www.rottentomatoes.com' + search_result['url'])
+            time.sleep(1)
+            return True
+        return False
+
+    def _click_rating(self, my_rating):
+        converted_rating = str(my_rating / 2)
+        if 'rating-root' not in self.site.browser.page_source:
+            return
+        movie_id = self.site.browser.find_element_by_id('rating-root').get_attribute('data-movie-id')
+
+        self.site.browser.execute_script("""
+            return $.post({
+                url: 'https://www.rottentomatoes.com/napi/user/rating',
+                data: { 
+                    'score': """ + converted_rating + """,
+                    'reviewText': '',
+                    'isSuperReviewer': false,
+                    'mediaType': 'movie',
+                    'mediaId': """ + movie_id + """
+                }
+            });
+        """)
+        time.sleep(1)
