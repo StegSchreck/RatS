e14a78c3c5d721958ffa66b562bc311e5ad2de08
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2021-02-11 19:25:09 +0100

    Letterboxd: Close privacy notice banner and video before attempting login - fixes #143

--- RatS/letterboxd/letterboxd_site.py
+++ RatS/letterboxd/letterboxd_site.py
@@ -1,3 +1,5 @@
+import time
+
 from RatS.base.base_site import Site
 
 
@@ -12,3 +14,25 @@ class Letterboxd(Site):
 
     def _get_login_page_url(self):
         return "https://letterboxd.com/sign-in/"
+
+    def _pre_login_action(self):
+        video_elements = self.browser.find_elements_by_id('tyche_trendi_video_container')
+        if len(video_elements) == 0:
+            return
+        video_element = video_elements[0]
+        if video_element is not None:
+            close_video_buttons = video_element.find_elements_by_id('pw-close-btn')
+            if close_video_buttons is not None and len(close_video_buttons) > 0:
+                close_video_buttons[0].click()
+                time.sleep(1)
+
+    def _handle_cookie_notice_if_present(self):
+        cookie_notices = self.browser.find_elements_by_id('tyche_cmp_modal')
+        if len(cookie_notices) == 0:
+            return
+        cookie_notice = cookie_notices[0]
+        if cookie_notice is not None:
+            cookie_accept_buttons = cookie_notice.find_elements_by_xpath("//*[contains(text(), 'Continue to Site')]")
+            if cookie_accept_buttons is not None and len(cookie_accept_buttons) > 0:
+                cookie_accept_buttons[0].click()
+                time.sleep(1)
