75ea2a5af20dfa6707744a3ad05913cb3adbc23a
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2022-10-08 15:13:05 +0200

    Handle Criticker Cookie Notice

--- RatS/criticker/criticker_site.py
+++ RatS/criticker/criticker_site.py
@@ -1,9 +1,12 @@
 from RatS.base.base_site import Site
+import time
+
+from selenium.webdriver.common.by import By
 
 
 class Criticker(Site):
     def __init__(self, args):
-        login_form_selector = "//form"
+        login_form_selector = "//*[@id='i_body']//form"
         self.LOGIN_USERNAME_SELECTOR = (
             login_form_selector + "//input[@id='si_username']"
         )
@@ -16,3 +19,16 @@ class Criticker(Site):
 
     def _get_login_page_url(self):
         return "https://www.criticker.com/signin.php"
+
+    def _handle_cookie_notice_if_present(self):
+        cookie_notices = self.browser.find_elements(By.ID, "i_cookies")
+        if len(cookie_notices) == 0:
+            return
+        cookie_notice = cookie_notices[0]
+        if cookie_notice is not None:
+            cookie_accept_buttons = cookie_notice.find_elements(
+                By.ID, "cookieagree"
+            )
+            if cookie_accept_buttons is not None and len(cookie_accept_buttons) > 0:
+                cookie_accept_buttons[0].click()
+                time.sleep(1)
